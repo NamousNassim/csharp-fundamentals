03c093f17ee275c5cb6175cda418c688d1bd58be
NamousNassim <nassimnamousdev@gmail.com>
2026-05-24 21:42:36 +0100

    Constant + multiple var declations

---
+++ 01-variables/Constants/Program.cs
@@ -0,0 +1,6 @@
+﻿const int speedOfLight = 299792458;
+const int x = 3 , y = 2 , z=4; // multiple variable declaration 
+
+Console.WriteLine($"{x},{y},{z}"); 
+
+Console.WriteLine(speedOfLight);// speedOfLight = 300000;  this is an error as the const variables must not change values

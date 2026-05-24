46389474dac7315b48806f896f972d80b4d13e6b
NamousNassim <nassimnamousdev@gmail.com>
2026-05-24 20:56:36 +0100

    first commit

---
+++ 01-variables/basics/HelloVariables.cs
@@ -0,0 +1,10 @@
+int age = 23;
+string name = "Nassim";
+decimal salary = 1000m;
+float height = 180f;
+bool isDeveloper = true;
+char grade = "A";
+
+
+Console.WriteLine($"My name is {name} my age is {age} my current salary is {salary}");
+Console.WriteLine($"DEVELOPER : {isDeveloper} i got my master with a {grade} grade and my height is {height}");

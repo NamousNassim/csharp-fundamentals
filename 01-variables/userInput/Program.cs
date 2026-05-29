a068832d9dc84e614e691673adc20cdd134907ce
NamousNassim <nassimnamousdev@gmail.com>
2026-05-29 19:11:46 +0100

    UserInput Project

---
+++ 01-variables/userInput/Program.cs
@@ -0,0 +1,19 @@
+﻿void getName()
+{
+    Console.WriteLine("Username : ");
+    string? userName = Console.ReadLine();
+    Console.WriteLine($"Username is {userName}");
+}
+
+void getAge()
+{
+    Console.WriteLine("Age : ");
+    int? age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Age is {age}");
+
+}
+
+
+
+getName();
+getAge();

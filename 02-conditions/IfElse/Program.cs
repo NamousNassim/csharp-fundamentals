840b342ab151ba5a1542a94f5bd2919c33d28c52
NamousNassim <nassimnamousdev@gmail.com>
2026-05-31 20:08:49 +0100

    conditions (if else if else)  +  short hand condition

---
+++ 02-conditions/IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿int x = 20;
+int y = 10;
+
+if (x > y)
+{
+    Console.WriteLine($"{x} is greater than {y}");
+}
+else
+{
+    Console.WriteLine($"{y} is greater than {x}");
+}
+
+int time = 20;
+string result = (time < 18) ? "good day" : "good evening";
+Console.WriteLine($"{result}");

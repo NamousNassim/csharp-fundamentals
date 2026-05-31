8a1fff94e73d8b4331611e6afa21ec3ff39c0ec3
NamousNassim <nassimnamousdev@gmail.com>
2026-05-31 19:59:04 +0100

    strings

---
+++ 01-variables/Strings/Program.cs
@@ -0,0 +1,11 @@
+﻿string greetings = "Hello";
+Console.WriteLine($"lenght of the word {greetings} is {greetings.Length}");
+
+Console.WriteLine($"Capital {greetings} is {greetings.ToUpper()}");
+Console.WriteLine($"Lower {greetings} is {greetings.ToLower()}");
+
+Console.WriteLine($"the second char of {greetings} is {greetings[1]}");
+
+char letter = 'o';
+
+Console.WriteLine($"position of the char o is {greetings.IndexOf(letter)}");
